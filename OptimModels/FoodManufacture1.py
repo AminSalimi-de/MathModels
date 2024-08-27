e71089389947974ba95d6856d69a10e1eb0d1f1d
AminSalimi <amin.salimi.de@gmail.com>
2024-08-27 18:35:18 +0200

    Defined Objective for FM1

--- OptimModels/FoodManufacture1.py
+++ OptimModels/FoodManufacture1.py
@@ -8,13 +8,23 @@ nMonths = 6
 
 JuneStorage = 500 #[ton]
 StorageUB = 1000 #[ton]
+MonthlyStorageCost = 5 #[pond/(ton.month)]
+ProductPrice = 150 # 150 [pond/ton]
 
 # [i][j] where i=Oil Type, j=Month index
 def Create2DArray(n1, n2):
-    return [[None for x in range(n2)] for y in range(n1)]
-def Create1DArray(size):
-    return [None for y in range(nMonths)]
+    return [[None for x2 in range(n2)] for x1 in range(n1)]
 
+def Create1DArray(size):
+    return [None for x1 in range(size)]
+
+def GetOilPrices():
+    Oil0Prices = [110, 130, 110, 120, 100, 90]
+    Oil1Prices = [120, 130, 140, 110, 120, 100]
+    Oil2Prices = [130, 110, 130, 120, 150, 140]
+    Oil3Prices = [110, 90, 100, 120, 110, 80]
+    Oil4Prices = [115, 115, 95, 125, 105, 135]
+    return [Oil0Prices, Oil1Prices, Oil2Prices, Oil3Prices, Oil4Prices]
 
 #       Decision Variables
 BV = Create2DArray(nOils, nMonths) # Buy Oils Variables 
@@ -40,11 +50,31 @@ for i in range(nOils):
 for i in range(nMonths):
     PV[i] = h.addVariable()
 
+#       Constraints
+
+
+
 
 #       Objective
-ProductPrice = 150 # 150 [pond/ton]
 Income = 0
 for monthlyProd in PV:    
     Income = ProductPrice*monthlyProd
+
+OilPurchaseCost = 0
+OilPrices = GetOilPrices()
+for i in range(nOils):
+    for j in range(nMonths):
+        OilPurchaseCost += OilPrices[i][j] * BV[i][j]
+
+OilStorageCost = 0
+for i in range(nOils):
+    for j in range(nMonths):
+        OilPurchaseCost += MonthlyStorageCost * SV[i][j]
+
+Cost = OilPurchaseCost + OilStorageCost
+
+Profit = Income - Cost
+
+h.maximize(Profit)
 
 print("end")
